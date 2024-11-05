74a0241d61b665173d732107d7b3f0a7c34f72c7
James Shore <jshore@jamesshore.com>
2024-11-04 21:25:44 -0800

    Create integrateAsync() abstraction for Repo tests

--- _build/tools/_repo_test.js
+++ _build/tools/_repo_test.js
@@ -5,29 +5,20 @@ import Shell from "infrastructure/shell.js";
 import ConsoleOutput from "infrastructure/console_output.js";
 import Colors from "infrastructure/colors.js";
 import TaskError from "tasks/task_error.js";
+import * as ensure from "util/ensure.js";
 
 export default test(({ describe }) => {
 
 	describe("integrate", ({ it }) => {
 
 		it("merges dev directory into integration directory", async () => {
-			const stdout = ConsoleOutput.createNull();
-			const stdoutTracker = stdout.track();
-			const repo = new Repo(Shell.createNull(), stdout);
-			const build = new BuildStub(stdout, true);
-
-			const config = {
+			const { stdoutTracker } = await integrateAsync({
+				buildPasses: true,
 				devBranch: "my_dev_branch",
 				integrationBranch: "my_integration_branch",
-			};
-			const message = "my integration message";
-
-			await repo.integrateAsync({
-				build,
 				buildTask: "my_task",
-				buildOptions: { myOptions: true },
-				config,
-				message,
+				buildOptions: { buildOptions: true },
+				message: "my integration message",
 			});
 
 			assert.equal(stdoutTracker.data, [
@@ -44,21 +35,9 @@ export default test(({ describe }) => {
 		});
 
 		it("runs build with provided task name and options", async () => {
-			const repo = new Repo(Shell.createNull(), ConsoleOutput.createNull());
-			const build = new BuildStub(ConsoleOutput.createNull(), true);
-
-			const config = {
-				devBranch: "my_dev_branch",
-				integrationBranch: "my_integration_branch",
-			};
-			const message = "my integration message";
-
-			await repo.integrateAsync({
-				build,
+			const { build } = await integrateAsync({
 				buildTask: "my_task",
 				buildOptions: { myOptions: true },
-				config,
-				message,
 			});
 
 			assert.equal(build.taskNames, [ "my_task" ], "build task names");
@@ -66,50 +45,20 @@ export default test(({ describe }) => {
 		});
 
 		it("fails gracefully if repo has uncommitted changes", async () => {
-			const shell = Shell.createNull({
-				"git status --porcelain": { stdout: "some changes" },
-			});
-			const stdout = ConsoleOutput.createNull();
-			const stdoutTracker = stdout.track();
-			const repo = new Repo(shell, stdout);
-			const build = new BuildStub(stdout, true);
-
-			const config = {
-				devBranch: "my_dev_branch",
-				integrationBranch: "my_integration_branch",
-			};
-
 			await assert.errorAsync(
-				() => repo.integrateAsync({
-					build,
-					buildTask: "irrelevant_task",
-					buildOptions: { irrelevantOptions: true },
-					config,
-					message: "irrelevant integration message",
+				() => integrateAsync({
+					shellOptions: {
+						"git status --porcelain": { stdout: "some changes" },
+					}
 				}),
 				"Commit changes before integrating",
 			);
 		});
 
 		it("fails gracefully if build fails", async () => {
-			const stdout = ConsoleOutput.createNull();
-			const stdoutTracker = stdout.track();
-			const repo = new Repo(Shell.createNull(), stdout);
-			const build = new BuildStub(stdout, false);
-
-			const config = {
-				devBranch: "my_dev_branch",
-				integrationBranch: "my_integration_branch",
-			};
-			const message = "my integration message";
-
 			await assert.errorAsync(
-				() => repo.integrateAsync({
-					build,
-					buildTask: "irrelevant_task",
-					buildOptions: { irrelevantOptions: true },
-					config,
-					message: "irrelevant integration message",
+				() => integrateAsync({
+					buildPasses: false,
 				}),
 				"Build error: Stub build failed",
 			);
@@ -129,13 +78,45 @@ export default test(({ describe }) => {
 
 });
 
-function assertCommands(shellTracker, expected) {
-	const actual = shellTracker.data.map(({ command, args }) => {
-		const quotedArgs = args.map((arg) => (arg.includes(" ") ? `'${arg}'` : arg));
-		return [ command, ...quotedArgs ].join(" ");
+async function integrateAsync({
+	devBranch = "irrelevant_dev_branch",
+	integrationBranch = "irrelevant_integration_branch",
+	buildTask = "irrelevant_build_task",
+	buildOptions = { irrelevantBuildOptions: true },
+	buildPasses = true,
+	message = "irrelevant_integration_message",
+	shellOptions,
+} = {}) {
+	ensure.signature(arguments, [[ undefined, {
+		devBranch: [ undefined, String ],
+		integrationBranch: [ undefined, String ],
+		buildTask: [ undefined, String ],
+		buildOptions: [ undefined, Object ],
+		buildPasses: [ undefined, Boolean ],
+		message: [ undefined, String ],
+		shellOptions: [ undefined, Object ],
+	}]]);
+
+	const stdout = ConsoleOutput.createNull();
+	const stdoutTracker = stdout.track();
+	const shell = Shell.createNull(shellOptions);
+	const repo = new Repo(shell, stdout);
+	const build = new BuildStub(stdout, buildPasses);
+
+	const config = {
+		devBranch,
+		integrationBranch,
+	};
+
+	await repo.integrateAsync({
+		build,
+		buildTask: buildTask,
+		buildOptions: buildOptions,
+		config,
+		message,
 	});
 
-	assert.equal(actual, expected);
+	return { stdoutTracker, build };
 }
 
 class BuildStub {
