facffe3e562200695dcb16dc723ef0d89a65e070
James Shore <jim.shore@opensesame.com>
2025-03-30 17:00:44 -0700

    Extract Runnable from BeforeAfter

--- src/ergotest/suite/before_after.ts
+++ src/ergotest/suite/before_after.ts
@@ -4,12 +4,9 @@ import { ItFn, ItOptions } from "../test_api.js";
 import { RunOptions } from "./test.js";
 import { RunData } from "./test_suite.js";
 import { RunResult, TestCaseResult } from "../results/test_result.js";
-import { runTestFnAsync } from "./runnable_function.js";
+import { Runnable, runTestFnAsync } from "./runnable_function.js";
 
-export class BeforeAfter {
-	private readonly _name: string[];
-	private readonly _options: ItOptions;
-	private readonly _fnAsync: ItFn;
+export class BeforeAfter extends Runnable {
 
 	static create({
 		name,
@@ -21,24 +18,6 @@ export class BeforeAfter {
 		fnAsync: ItFn,
 	}) {
 		return new BeforeAfter(name, options, fnAsync);
-	}
-
-	constructor(name: string[], options: ItOptions, fnAsync: ItFn) {
-		this._name = name;
-		this._options = options;
-		this._fnAsync = fnAsync;
-	}
-
-	get name(): string[] {
-		return this._name;
-	}
-
-	get options(): ItOptions {
-		return this._options;
-	}
-
-	get fnAsync(): ItFn {
-		return this._fnAsync;
 	}
 
 	async runBeforeAfterAllAsync(runOptions: RunOptions, runData: RunData) {
@@ -57,7 +36,7 @@ export class BeforeAfter {
 	async _runAsyncInternal(runOptions: RunOptions, runData: RunData) {
 		if (runData.skipAll) return RunResult.skip({ name: this.name, filename: runData.filename });
 
-		return await runTestFnAsync(this.name, this.fnAsync, this.options.timeout, runOptions, runData);
+		return await this._runTestFnAsync(runOptions, runData);
 	}
 
 }
--- src/ergotest/suite/runnable_function.ts
+++ src/ergotest/suite/runnable_function.ts
@@ -1,9 +1,71 @@
 // Copyright Titanium I.T. LLC. License granted under terms of "The MIT License."
 import { Milliseconds, RunOptions } from "./test.js";
 import { RunResult, TestMark } from "../results/test_result.js";
-import { ItFn } from "../test_api.js";
+import { ItFn, ItOptions } from "../test_api.js";
 import { RunData } from "./test_suite.js";
 
+
+export class Runnable {
+
+	private readonly _name: string[];
+	private readonly _options: ItOptions;
+	private readonly _fnAsync: ItFn;
+
+	constructor(name: string[], options: ItOptions, fnAsync: ItFn) {
+		this._name = name;
+		this._options = options;
+		this._fnAsync = fnAsync;
+	}
+
+	get name(): string[] {
+		return this._name;
+	}
+
+	get options(): ItOptions {
+		return this._options;
+	}
+
+	get fnAsync(): ItFn {
+		return this._fnAsync;
+	}
+
+	async _runTestFnAsync(
+		runOptions: RunOptions,
+		runData: RunData,
+	): Promise<RunResult> {
+		const timeout = this._options.timeout ?? runData.timeout;
+
+		return await runOptions.clock.timeoutAsync(timeout, async () => {
+			try {
+				await this._fnAsync({ getConfig });
+				return RunResult.pass({
+					name: this._name,
+					filename: runData.filename
+				});
+			}
+			catch (error) {
+				return RunResult.fail({
+					name: this._name,
+					filename: runData.filename,
+					error,
+					renderError: runOptions.renderError
+				});
+			}
+		}, async () => {
+			return await RunResult.timeout({
+				name: this._name,
+				filename: runData.filename,
+				timeout: runData.timeout
+			});
+		});
+
+		function getConfig<T>(name: string) {
+			if (runOptions.config[name] === undefined) throw new Error(`No test config found for name '${name}'`);
+			return runOptions.config[name] as T;
+		}
+	}
+
+}
 
 export async function runTestFnAsync(
 	name: string[],
